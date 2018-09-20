c1c69f541e816f7a43c537114e562cb65ca4b350
PhilipUDE <36899848+PhilipUDE@users.noreply.github.com>
2018-09-20 16:13:17 +0200

    DayNightCycle

---
+++ Labyrinth_Local/Assets/skripts/DayNightCicle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DayNightCicle : MonoBehaviour {
+
+    private GameObject Sun;
+    private GameObject Moon;
+
+	// Use this for initialization
+	void Start () {
+        Sun = GameObject.Find("Sun");
+        Moon = GameObject.Find("Moon");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Sun.transform.RotateAround(Vector3.zero, Vector3.right, 0.5f*Time.deltaTime);
+        Sun.transform.LookAt(Vector3.zero);
+        Moon.transform.RotateAround(Vector3.zero, Vector3.right, 0.5f*Time.deltaTime);
+        Moon.transform.LookAt(Vector3.zero);
+    }
+}
